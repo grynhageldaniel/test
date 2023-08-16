98558efec408db5c7d642c3bf5fc42642a4c5ccf
grynhageldaniel <142397440+grynhageldaniel@users.noreply.github.com>
2023-08-16 12:17:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import math
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
-import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
 AgGrid(df)
