12f45e594e699384b6a68f49219dc481b181abf5
grynhageldaniel <142397440+grynhageldaniel@users.noreply.github.com>
2023-08-16 12:40:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid
 
-df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
-AgGrid(df)
 
 
 with st.echo(code_location='below'):
